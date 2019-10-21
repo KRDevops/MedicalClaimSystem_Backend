0b21ac2ee15449602f720c335fd849126dd43c86
AVK Kiran Kumar <avk.kirankumar@gmail.com>
2019-10-21 16:47:49 +0530

    Initial Setup

---
+++ src/main/java/com/hcl/mediclaim/exception/MediClaimException.java
@@ -0,0 +1,9 @@
+package com.hcl.mediclaim.exception;
+
+public class MediClaimException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public MediClaimException(String message) {
+		super(message);
+	}
+}
